518b244be3bacf1f8ae5579c1cb974c1ad165666
CDA <cathyda97@gmail.com>
2022-10-24 18:25:10 -0400

    connected to MongoDB

--- server/config/app.js
+++ server/config/app.js
@@ -25,7 +25,7 @@ mongoose.connect(MongoURI);
 const db = mongoose.connection;
 
 // Listen for Connections or Errors
-db.on('open', () => console.log(`Connected to MongoDB at Localhost`));
+db.on('open', () => console.log(`Connected to MongoDB`));
 db.on('error', () => console.error('Connection Error'));
 
 // view engine setup
--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,2 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://cda:cda123@cluster0.stkxej1.mongodb.net/midterm?retryWrites=true&w=majority";
 export const Secret = "someSecret";
